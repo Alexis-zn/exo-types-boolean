ea8113ccd05d97fad5cd0aa1638911ad4ac32bd8
Alexis-zn <alexis.zentici@gmail.com>
2021-02-15 16:56:55 +0100

    Exo done

---
+++ coat.js
@@ -0,0 +1,12 @@
+
+let isRainy = true
+let isWindy = false
+let temperature = 25
+
+if (temperature <= 15 || isRainy || isWindy) {
+  console.log('put on your coat') 
+  } 
+   else {
+    console.log('Dont put on your coat')
+  }
+ 
