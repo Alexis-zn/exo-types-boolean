cc58344e5c500794b590254b385c33f2d3021fe9
Alexis-zn <alexis.zentici@gmail.com>
2021-02-15 16:32:26 +0100

    Exo done

---
+++ trading.js
@@ -0,0 +1,10 @@
+let btc = 10000 ;  
+
+if (btc <= 20000) {
+  console.log('you should buy btc')
+} else if (btc >= 45000) {
+  console.log('you should sell or hold btc')
+} else {
+  console.log('you should just hold btc')
+}
+
